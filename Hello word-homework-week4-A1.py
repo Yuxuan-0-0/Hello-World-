7294f84e3341f4cbe7c742268691063e13d15a23
Yuxuan <72874585+Yuxuan-0-0@users.noreply.github.com>
2020-10-28 23:04:04 +0800

    Add files via upload

---
+++ Hello word-homework-week4-A1.py
@@ -0,0 +1,31 @@
+# Hello word - homework - week 4
+# Yuxuan
+# yhou10@sva.edu
+
+# Assignment 1
+
+import turtle 
+
+spiralsquare = turtle.Turtle()
+
+spiralsquare.speed(10)
+
+for i in range(140):
+    if i<=20:
+        spiralsquare.pencolor("red")
+    elif 20<i<=40:
+        spiralsquare.pencolor("orange")
+    elif 40<i<=60:
+        spiralsquare.pencolor("yellow")
+    elif 60<i<=80:
+        spiralsquare.pencolor("green")
+    elif 80<i<=100:
+        spiralsquare.pencolor("cyan")
+    elif 100<i<=120:
+        spiralsquare.pencolor("blue")
+    elif 120<i<=140:
+        spiralsquare.pencolor("purple")
+    spiralsquare.forward(i*2)
+    spiralsquare.right(90)
+
+turtle.done()
