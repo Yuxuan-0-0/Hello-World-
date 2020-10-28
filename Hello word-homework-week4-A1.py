fccdabb18d5ec18f134ba553e5d6908d70419ab2
Yuxuan <72874585+Yuxuan-0-0@users.noreply.github.com>
2020-10-28 23:22:27 +0800

    Add files via upload

--- Hello word-homework-week4-A1.py
+++ Hello word-homework-week4-A1.py
@@ -8,7 +8,7 @@ import turtle
 
 spiralsquare = turtle.Turtle()
 
-spiralsquare.speed(10)
+spiralsquare.speed(20)
 
 for i in range(140):
     if i<=20:
@@ -27,5 +27,42 @@ for i in range(140):
         spiralsquare.pencolor("purple")
     spiralsquare.forward(i*2)
     spiralsquare.right(90)
+
+for i in range(140):
+    if i<=20:
+        spiralsquare.pencolor("red")
+    elif 20<i<=40:
+        spiralsquare.pencolor("orange")
+    elif 40<i<=60:
+        spiralsquare.pencolor("yellow")
+    elif 60<i<=80:
+        spiralsquare.pencolor("green")
+    elif 80<i<=100:
+        spiralsquare.pencolor("cyan")
+    elif 100<i<=120:
+        spiralsquare.pencolor("blue")
+    elif 120<i<=140:
+        spiralsquare.pencolor("purple")
+    spiralsquare.forward(i*2)
+    spiralsquare.right(90)
+
+for i in range(140):
+    if i<=20:
+        spiralsquare.pencolor("red")
+    elif 20<i<=40:
+        spiralsquare.pencolor("orange")
+    elif 40<i<=60:
+        spiralsquare.pencolor("yellow")
+    elif 60<i<=80:
+        spiralsquare.pencolor("green")
+    elif 80<i<=100:
+        spiralsquare.pencolor("cyan")
+    elif 100<i<=120:
+        spiralsquare.pencolor("blue")
+    elif 120<i<=140:
+        spiralsquare.pencolor("purple")
+    spiralsquare.forward(i*2)
+    spiralsquare.right(90)
+
 
 turtle.done()
